7a40da37ac1456c063f2e52ba57c6a1dddfd9f20
Sanyog kumar <sanyogcgc@gmail.com>
2020-11-15 08:10:24 +0530

    React Test application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        Hi , This is my first application.
+      </header>
+      <body>
+Test body.
+        
+        </body>
+    </div>
+  );
+}
+
+export default App;
